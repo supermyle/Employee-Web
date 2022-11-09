8afc39c760a285b3d482d1789323521d417e47d2
supermyle <kylemanley99@gmail.com>
2022-11-08 21:30:42 -0500

    Added employees navbar tab and changed folder name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EmployeesListComponent } from './ui-components/employees/employees-list/employees-list.component';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { EmployeesListComponent } from './ui-components/employees/employees-list
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
