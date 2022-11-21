16532fd842da1fa0fbfd4f59948a0a7bc764abea
supermyle <kylemanley99@gmail.com>
2022-11-20 22:34:17 -0500

    Added edit employee form

--- src/app/components/edit-employee/edit-employee.component.ts
+++ src/app/components/edit-employee/edit-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from 'src/app/models/employee.model';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-edit-employee',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditEmployeeComponent implements OnInit {
 
-  constructor() { }
+  employeeDetails: Employee = {
+    id: '',
+    name: '',
+    email: '',
+    phone: '',
+    salary: '',
+    department: ''
+  };
+
+  constructor(private route: ActivatedRoute, private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get('id');
+
+        if (id) {
+          this.employeeService.getEmployee(id).subscribe({
+            next: (data) => {
+              this.employeeDetails = data;   
+            },
+            error: (response) => {
+              console.error(response);
+            }
+          });
+        }
+      }
+    });
   }
 
+  updateEmployee() {
+
+  }
 }
